5f3cef82792056d5d145c6866156b9aa98b1d5eb
Doruk Ege Aksu <dorukaksu912@gmail.com>
2024-11-26 19:42:26 +0300

    Java file

---
+++ Repository.java
@@ -0,0 +1,32 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Repository {
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/cmpe343";
+        String username = "root";
+        String password = "admin";
+
+        try (Connection connection = DriverManager.getConnection(url, username, password)) {
+            System.out.println("Connected to the database!");
+
+            // Example Query
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM users");
+
+            while (resultSet.next()) {
+                System.out.println("Customer ID: " + resultSet.getInt("user_id"));
+                System.out.println("First Name: " + resultSet.getString("first_name"));
+                System.out.println("Email: " + resultSet.getString("email"));
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+//javac Repository.java
+//java -cp ".;mysql-connector-j-9.1.0.jar" Repository
