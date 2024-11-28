3c9033e8d4ea1b3f0497b3b09c7f77eae85bb4e7
Kerem <kerembicen94@gmail.com>
2024-11-28 15:35:50 +0300

    Added classes

---
+++ Engineer.java
@@ -0,0 +1,12 @@
+import java.sql.Date;
+
+public class Engineer extends User {
+
+    public Engineer(int userId, String username, String password, String role, String firstName, String lastName, String phoneNo, String email, Date dateOfBirth, Date dateOfStart) {
+        super(userId, username, password, role, firstName, lastName, phoneNo, email, dateOfBirth, dateOfStart);
+    }
+    
+    public boolean menu() {
+        return false;
+    }
+}
---
+++ Intern.java
@@ -0,0 +1,12 @@
+import java.sql.Date;
+
+public class Intern extends User {
+
+    public Intern(int userId, String username, String password, String role, String firstName, String lastName, String phoneNo, String email, Date dateOfBirth, Date dateOfStart) {
+        super(userId, username, password, role, firstName, lastName, phoneNo, email, dateOfBirth, dateOfStart);
+    }
+    
+    public boolean menu() {
+        return false;
+    }
+}
