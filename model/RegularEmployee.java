881edd8a91aca33de5085f13a3cc2c39fa08bb34
Doruk Ege Aksu <dorukaksu912@gmail.com>
2024-12-03 13:29:40 +0300

    Completed employee menu

--- model/RegularEmployee.java
+++ model/RegularEmployee.java
@@ -5,15 +5,15 @@ import java.util.Scanner;
 
 public abstract class RegularEmployee extends User{
 
+    Scanner scanner = new Scanner(System.in);
+
     public RegularEmployee(int userId, String username, String password, String role, String firstName, String lastName, String phoneNo, String email, Date dateOfBirth, Date dateOfStart) {
         super(userId, username, password, role, firstName, lastName, phoneNo, email, dateOfBirth, dateOfStart);
     }
 
     public boolean menu() {
-        System.out.println("[" + this.getRole() + " MENU]");
+        System.out.println("[EMPLOYEE MENU]");
         System.out.println("Logged in as " + this.getUsername() + "\n");
-
-        Scanner scanner = new Scanner(System.in);
         String input;
 
         if (this.getPassword().equals("password")) {
@@ -27,48 +27,39 @@ public abstract class RegularEmployee extends User{
         }
 
         System.out.println("Some useful commands:");
-        System.out.println("1. display: Displays your profile");
-        System.out.println("2. update <field>: Updates information");
-        System.out.println("3. logout: Returns to login screen");
+        System.out.println("[1] Display Profile");
+        System.out.println("[2] Display Detailed Profile");
+        System.out.println("[3] Update Profile");
+        System.out.println("[4] Logout");
         System.out.println();
 
         System.out.println("Enter command: ");
         input = scanner.nextLine().trim().toLowerCase();
 
         switch (input) {
-            case "display":
             case "1":
+            case "display":
                 System.out.println("\n" + this);
                 System.out.println("Press enter to continue...");
                 scanner.nextLine();
                 clearScreen();
                 break;
-            case "update username":
-                System.out.println("Enter new username:");
-                input = scanner.nextLine().trim().toLowerCase();
-                this.setUsername(input);
-                System.out.println("Username updated.");
+            case "2":
+            case "detailed":
+                System.out.println("\n" + this.detailedProfile());
+                System.out.println("Press enter to continue...");
+                scanner.nextLine();
+                clearScreen();
                 break;
-            case "update password":
-                System.out.println("Enter new password:");
-                input = scanner.nextLine().trim();
-                this.setPassword(input);
-                System.out.println("Password updated.");
-                break;
-            case "update first name":
-                System.out.println("Enter new first name:");
-                input = scanner.nextLine().trim();
-                this.setFirstName(input);
-                System.out.println("First name updated.");
-                break;
-            case "update last name":
-                System.out.println("Enter new last name:");
-                input = scanner.nextLine().trim();
-                this.setLastName(input);
-                System.out.println("Last name updated.");
-                break;
-            case "logout":
             case "3":
+            case "update":
+                boolean flag = true;
+                while(flag){
+                    flag = updateMenu();
+                }
+                break;
+            case "4":
+            case "logout":
                 System.out.println("Logged out.");
                 return false;
             default:
@@ -77,5 +68,68 @@ public abstract class RegularEmployee extends User{
                 break;
         }
         return true;
+    }
+
+    private String detailedProfile() {
+        return "User ID: " + this.getUserId() + "\n" +
+               "Username: " + this.getUsername() + "\n" +
+               "Role: " + this.getRole() + "\n" +
+               "First Name: " + this.getFirstName() + "\n" +
+               "Last Name: " + this.getLastName() + "\n" +
+               "Phone Number: " + this.getPhoneNo() + "\n" +
+               "Email: " + this.getEmail() + "\n" +
+               "Date of Birth: " + this.getDateOfBirth() + "\n" +
+               "Date of Start: " + this.getDateOfStart() + "\n";
+    }
+
+    private boolean updateMenu() {
+        System.out.println("Update Profile");
+        System.out.println("Choose what to update:");
+        System.out.println("[1] Password");
+        System.out.println("[2] Phone Number");
+        System.out.println("[3] Email");
+        System.out.println("[4] Exit");
+        System.out.println();
+
+        System.out.println("Enter command: ");
+        String input = scanner.nextLine().trim().toLowerCase();
+
+        switch (input) {
+            case "1":
+                System.out.println("Enter new password:");
+                input = scanner.nextLine().trim();
+                this.setPassword(input);
+                System.out.println("Password updated.");
+                System.out.println("\nPress enter to continue...");
+                scanner.nextLine();
+                clearScreen();
+                break;
+            case "2":
+                System.out.println("Enter new phone number:");
+                input = scanner.nextLine().trim();
+                this.setPhoneNo(input);
+                System.out.println("Phone number updated.");
+                System.out.println("\nPress enter to continue...");
+                scanner.nextLine();
+                clearScreen();
+                break;
+            case "3":
+                System.out.println("Enter new email:");
+                input = scanner.nextLine().trim();
+                this.setEmail(input);
+                System.out.println("Email updated.");
+                System.out.println("\nPress enter to continue...");
+                scanner.nextLine();
+                clearScreen();
+                break;
+            case "4":
+                clearScreen();
+                return false;
+            default:
+                clearScreen();
+                System.out.println("Invalid command.");
+                break;
+        }
+        return false;
     }
 }
