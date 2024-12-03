d6325cc57af8dd870cc5f6c095efdd1129c4472d
Tags242 <tbilen@stu.khas.edu.tr>
2024-12-03 13:56:51 +0300

    Heap sort, insertion sort, ASCII art

--- util/Algorithm.java
+++ util/Algorithm.java
@@ -116,4 +116,65 @@ public class Algorithm {
             }
         }
     }
+
+    public static void heapSort(int[] array) 
+    {
+        int n = array.length;
+        for (int i = n / 2 - 1; i >= 0; i--) 
+        {
+            heapify(array, n, i);
+        }
+        for (int i = n - 1; i > 0; i--) 
+        {
+            int temp = array[0];
+            array[0] = array[i];
+            array[i] = temp;
+            heapify(array, i, 0);
+        }
+    }
+
+    private static void heapify(int[] array, int n, int i) 
+    {
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+
+        if (left < n && array[left] > array[largest]) 
+        {
+            largest = left;
+        }
+
+        if (right < n && array[right] > array[largest]) 
+        {
+            largest = right;
+        }
+
+        if (largest != i) 
+        {
+            int swap = array[i];
+            array[i] = array[largest];
+            array[largest] = swap;
+
+            heapify(array, n, largest);
+        }
+    }
+
+    public static void insertionSort(int[] array) 
+    {
+        int n = array.length;
+
+        for (int i = 1; i < n; i++) 
+        {
+            int key = array[i];
+            int j = i - 1;
+
+            while (j >= 0 && array[j] > key) 
+            {
+                array[j + 1] = array[j];
+                j--;
+            }
+
+            array[j + 1] = key;
+        }
+    }
 }
